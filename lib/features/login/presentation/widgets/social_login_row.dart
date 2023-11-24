28fc82e84aec60986a03326be07ed56f7f27d2ba
malekkhasawneh <malekmamoon341@gmail.com>
2023-11-24 23:20:12 +0300

    create facebook auth

--- lib/features/login/presentation/widgets/social_login_row.dart
+++ lib/features/login/presentation/widgets/social_login_row.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:prestige_valet_app/core/resources/images.dart';
 class SocialLoginRow extends StatelessWidget {
@@ -30,6 +31,18 @@ class SocialLoginRow extends StatelessWidget {
     }
   }
 
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -44,7 +57,7 @@ class SocialLoginRow extends StatelessWidget {
         ),
         GestureDetector(
           onTap: () async {
-            await signInWithGoogle().then((value) {
+            await signInWithFacebook().then((value) {
               log('================================================= displayName ${value.user!.displayName}');
               log('================================================= email ${value.user!.email}');
               log('================================================= photoURL ${value.user!.photoURL}');
