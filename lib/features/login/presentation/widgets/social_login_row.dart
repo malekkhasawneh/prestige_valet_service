97d926cbdc4502b36c3ce53f205df34c21a674f4
malekkhasawneh <malekmamoon341@gmail.com>
2023-12-01 19:47:45 +0300

    fix twitter auth issue

--- lib/features/login/presentation/widgets/social_login_row.dart
+++ lib/features/login/presentation/widgets/social_login_row.dart
@@ -1,10 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:prestige_valet_app/core/resources/images.dart';
 import 'package:prestige_valet_app/features/login/presentation/cubit/login_cubit.dart';
-import 'package:prestige_valet_app/features/sign_up/presentation/cubit/sign_up_cubit.dart';
-
+import 'package:twitter_login/twitter_login.dart';
+import 'dart:developer';
 class SocialLoginRow extends StatelessWidget {
-  SocialLoginRow({super.key});
+  const SocialLoginRow({super.key});
+
+  Future<UserCredential> signInWithTwitter() async {
+    // Create a TwitterLogin instance
+    final twitterLogin = TwitterLogin(
+        apiKey: '2wB67cUC5uGGxHazhe1UToIlV',
+        apiSecretKey: '3UoLoJoqNvPqumQWfp6NUsgBFWxrviUD9KlZgo68AbQMso30b7',
+        redirectURI: 'https://prestige-valet-service.firebaseapp.com/__/auth/handler');
+
+    // Trigger the sign-in flow
+    final authResult = await twitterLogin.login();
+
+    // Create a credential from the access token
+    final twitterAuthCredential = TwitterAuthProvider.credential(
+      accessToken: authResult.authToken!,
+      secret: authResult.authTokenSecret!,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance
+        .signInWithCredential(twitterAuthCredential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +35,21 @@ class SocialLoginRow extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Image.asset(
-          Images.twitterLogo,
-          width: screenWidth * 0.15,
-          height: screenHeight * 0.15,
+        GestureDetector(
+          onTap: () async {
+            await signInWithTwitter().then((value){
+              log('=============================================== Here ${value.user!.displayName}');
+            });
+          },
+          child: Image.asset(
+            Images.twitterLogo,
+            width: screenWidth * 0.15,
+            height: screenHeight * 0.15,
+          ),
         ),
         GestureDetector(
           onTap: () {
-           LoginCubit.get(context).loginWithGoogle();
+            LoginCubit.get(context).loginWithGoogle();
           },
           child: Image.asset(
             Images.gmailLogo,
