2cca747b7181cba7dadc6bb12dbe5671d11e8daa
malekkhasawneh <malekmamoon341@gmail.com>
2024-05-14 23:51:42 +0300

    add missing functions

--- lib/features/splash/presentation/cubit/splash_cubit.dart
+++ lib/features/splash/presentation/cubit/splash_cubit.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 import 'dart:developer';
 
-import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,6 +8,7 @@ import 'package:prestige_valet_app/features/splash/domain/usecase/check_is_user_
 import 'package:prestige_valet_app/features/splash/domain/usecase/get_is_first_time_usecae.dart';
 import 'package:prestige_valet_app/features/splash/domain/usecase/is_user_usecase.dart';
 import 'package:prestige_valet_app/features/splash/domain/usecase/set_is_first_time_usecase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'splash_state.dart';
 
@@ -80,31 +79,14 @@ class SplashCubit extends Cubit<SplashState> {
     }
   }
 
-  bool isUserBlocked = false;
+  Future<bool> isFirstTimeOpenTheApp() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    log('===================================== oo ${prefs.getBool('IsFirstTimeOpenTheApp') ?? true}');
+    return prefs.getBool('IsFirstTimeOpenTheApp') ?? true;
+  }
 
-  Future<void> isBlocked() async {
-    try {
-      final String basicAuth =
-          'Basic ${base64Encode(utf8.encode('malek_mamoon123456456:!@#QWE123qwe'))}';
-
-      Map<String, String> myHeaders = {'authorization': basicAuth};
-      Dio dio = Dio(BaseOptions(headers: myHeaders));
-      Response response = await dio.get(
-        'https://doctoral-nation.000webhostapp.com/block_user.php',
-      );
-      if (response.statusCode == 200) {
-        Map<String, dynamic> data = json.decode(response.data);
-        log('======================================== ${data['data']['isBlocked']}');
-        if (data['data']['isBlocked'] == '1') {
-          isUserBlocked = true;
-        } else {
-          isUserBlocked = false;
-        }
-      } else {
-        isUserBlocked = false;
-      }
-    } on Exception {
-      isUserBlocked = false;
-    }
+  Future<void> setIsFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('IsFirstTimeOpenTheApp', false);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import flutter_secure_storage_macos
 import network_info_plus
 import path_provider_foundation
 import rive_common
+import shared_preferences_foundation
 import sqflite
 import twitter_login
 
@@ -31,6 +32,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   TwitterLoginPlugin.register(with: registry.registrar(forPlugin: "TwitterLoginPlugin"))
 }
